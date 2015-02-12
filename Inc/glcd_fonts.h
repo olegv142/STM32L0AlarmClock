1b0bf483d3db39d0696ff56b946e842ba618ac16
Oleg Volkov <olegv@parallels.com>
2015-02-12 09:06:05 +0300

    Fonts again

--- Inc/glcd_fonts.h
+++ Inc/glcd_fonts.h
@@ -7,11 +7,15 @@
  */
 
 /* Cyrillic font */
-extern struct glcd_font g_font_Lucida12x12;
+extern struct glcd_font g_font_Lucida12x12Cyr;
 
 /* Latin font */
-extern struct glcd_font g_font_Lucida_Console13x20;
+extern struct glcd_font g_font_Tahoma19x20;
 
-/* Large fonts with only 0..9: symbols for displaying time */
-extern struct glcd_font g_font_Tahoma29x48;
-extern struct glcd_font g_font_Tahoma33x52;
+/* Small bold Latin font */
+extern struct glcd_font g_font_Tahoma12x11Bld;
+
+/* Fonts with only 0..9: symbols for displaying time */
+extern struct glcd_font g_font_Tahoma9x12Clk;
+extern struct glcd_font g_font_Tahoma29x48Clk;
+extern struct glcd_font g_font_Tahoma33x52Clk;
--- Src/main.c
+++ Src/main.c
@@ -126,9 +126,11 @@ int main(void)
 
       get_time_str(&g_clock, str);
       BSP_EPD_Clear(EPD_COLOR_WHITE);
-      glcd_print_str(0,  15, tvs.v_str, &g_font_Lucida12x12, 1);
-      glcd_print_str(64, 13, tvs.t_str, &g_font_Lucida_Console13x20, 0);
-      glcd_print_str_r(LCD_WIDTH, 0, str, &g_font_Tahoma29x48, 6);
+      glcd_print_str(0,  15, "00:00",   &g_font_Tahoma9x12Clk, 2);
+      glcd_print_str(0,  12, tvs.v_str, &g_font_Tahoma12x11Bld, 2);
+      glcd_print_str(55, 13, tvs.t_str, &g_font_Tahoma19x20, 1);
+      glcd_print_str_r(LCD_WIDTH, 13, "86%", &g_font_Tahoma19x20, 1);
+      glcd_print_str_r(LCD_WIDTH, 0, str, &g_font_Tahoma29x48Clk, 6);
       BSP_EPD_RefreshDisplay();
       g_clock_updated = 0;
     }
